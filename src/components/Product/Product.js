4854e533414a0301bd6fa2b846d4a555b6003a36
anikaaurony <aurony111@gmail.com>
2022-05-20 13:20:43 +0600

    data modify done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import './Product.css';
 
 const Product = ({item}) => {
-    const {id, name, price, quantity, supplier} = item;
+    const {id, name, price, quantity, supplier, img, des} = item;
     const navigate = useNavigate();
     const navigateToInventory = id => {
         navigate(`/inventory/${id}`)
@@ -11,12 +11,12 @@ const Product = ({item}) => {
     return (
         <div className='item'>
             
-            <img src="" alt=""/>
+            <img className='pic' src={img} alt=""/>
             <h2>{name}</h2>
             <h4>Price:{price}</h4>
             <h4>Quantity:{quantity}</h4>
             <h5>Supplier:{supplier}</h5>
-            <p>{"lorem "}</p>
+            <p>{des}</p>
             <button onClick={() => navigateToInventory(id)} className='button'>Update</button>
         </div>
     );
